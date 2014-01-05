78b903501077a5fff3b2d40e9a6f92efea0d823f
Jiří Zůna <jiri@zunovi.cz>
2014-01-05 18:48:35 +0100

    go through the directory and print all exif info
    - which can be obtained by scanning through the jpeg files inside

--- src/zexif/core.cljs
+++ src/zexif/core.cljs
@@ -5,21 +5,37 @@
 
 (def fs (node/require "fs"))
 (def walk (node/require "walk"))
+(def extractif (node/require "extractif"))
 
 (def ops (js-obj)) 
 (set! (.-followLinks true))
 
-(defn snif-exif [file out]
-  )
+(defn snif-exif [file]
+  (let [out (chan)]
+    (extractif file
+               (fn [err exif]
+                 (go
+                  (>! out {:file file :err err :exif exif}))))
+    out))
+
+(defn clojurize-exif [src]
+  (when-let [exif (:exif src)]
+    (assoc src :exif (js->clj exif :keywordize-keys true))))
 
 (defn main []
-  (let [walker (.walk walk "/tmp" ops)
-        files (chan)]
+  (let [walker (.walk walk "/Volumes/data/Alba" ops)
+        files (chan)
+        exif-data (chan 500)]
     (.on walker "file" (fn [root stats next]
-                         (go (>! files stats))
-                         (next)))
-    (go (loop [stat (<! files)]
-          (.log js/console (str "File " (.-name stat)))
-          (recur (<! files))))))
+                         (go (>! files {:root root :stat stats})
+                             (next))))
+    (go (loop [{:keys [root stat]} (<! files)]
+          (let [file (str root "/" (.-name stat))
+                exif-ch (snif-exif file)
+                data (<! exif-ch)
+                m (clojurize-exif data)]
+            (prn m)
+;;            (>! exif-data data)
+            (recur (<! files)))))))
 
 (set! *main-cli-fn* main)
